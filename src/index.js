88d0241aa9e2ef08ebf16849ea68f1d462f8dbeb
Zara Khedri <zahrakhedri.94@gmail.com>
2023-10-05 18:01:02 +0330

    add js - show current temperature

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+function showTemperature(response) {
+  let cityName = document.querySelector("h1");
+  let temperatureElement = document.querySelector("#temperature");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  cityName.innerHTML = response.data.name;
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "85bbd3d16a2dfe0ecf253c7ae1e8fe03";
+let city = "london";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(showTemperature);
