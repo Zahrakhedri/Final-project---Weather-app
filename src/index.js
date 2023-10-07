369f2e7fb6602ce86b0be6896e6a064157aecdd8
Zara Khedri <zahrakhedri.94@gmail.com>
2023-10-07 15:25:09 +0330

    remove unit conversion

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ function displayForecast(response) {
   let weatherForecast = document.querySelector("#forecast");
   let forecastHtml = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
+    if (index < 5) {
       forecastHtml =
         forecastHtml +
         `    <div class="col-2">
@@ -72,7 +72,6 @@ function showTemperature(response) {
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
-  celciusTemperature = response.data.main.temp;
   cityName.innerHTML = response.data.name;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   descriptionElement.innerHTML = response.data.weather[0].description;
@@ -96,36 +95,9 @@ function handleSubmit(event) {
   event.preventDefault();
   let inputElement = document.querySelector("#city-input");
   search(inputElement.value);
-  celcuisLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
 }
 
 let formElement = document.querySelector("#search-form");
 formElement.addEventListener("submit", handleSubmit);
-
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  fahrenheitLink.classList.add("active");
-  celcuisLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temperature");
-  let fahrenheitTemperature = (celciusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
-function displayCelciusTemperature(event) {
-  event.preventDefault();
-  celcuisLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celciusTemperature);
-}
-
-let celciusTemperature = null;
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celcuisLink = document.querySelector("#celcius-link");
-celcuisLink.addEventListener("click", displayCelciusTemperature);
 
 search("san francisco");
